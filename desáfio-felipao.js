bc24209f73f39a2c7c957a60a63ae6ad52c7325e
RHUAN NUNES DIAS <144816206+RhuanLee@users.noreply.github.com>
2023-10-04 17:14:16 -0300

    Update desáfio-felipao.js

--- desáfio-felipao.js
+++ desáfio-felipao.js
@@ -1,1 +1,23 @@
+console.log("Olá, héroi meu nome é RADIANTE MESTRE Aqui eu consigo ver seu elo!")
+let xp = 100
+let heroi = "LUCAS"
+let nivel = "PASSO DE RADIANTE QUE NÃO CONSIGO NEM SOMAR!"
+
+if(xp <= 1001 ){
+    console.log("Aquí você só mais no meio de vários elo inicial FERRO")
+}else if (xp< 2001){
+    console.log("Level UP BRONZE")
+}else if(xp <= 3001){
+    console.log("WOOW CONtinue assim PRATA")
+}else if(xp <=4001){
+    console.log("EITA QUE AGORA EVOLUÇÃO NÃO PARA OURO")
+}else if(xp <= 5001){
+    console.log("CHEGA QUE JÁ DEU NÉ ? !PLATINA!")
+}else if(xp <= 6001) {
+    console.log("VICIADO JÁ ARRUMO CASA PELOMENOS !!!ASCENDENTE!!!")
+}else if(xp <= 7001){
+    console.log("OLOKO BICHO DEVE NEM SAI DESTE PC #!IRADIANTEI!#")
+}else{
+    console.log("PARABÉNS DESEMPREGADO"+ heroi +"NÃO CONSIGO MAIS NEM SOMAR" + nivel)
+}
 
